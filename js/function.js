19c68d316546af2c50a81e7a94b835784de183e3
masuda8row <masuda8row@gmail.com>
2019-12-19 17:53:13 +0900

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,161 +1,112 @@
+'use strict';
+
 const thisYear = document.getElementById('this-year');
 const thisMonth = document.getElementById('this-month');
-const thisWeek = document.getElementById('this-week');
 const today = document.getElementById('today');
 const prev = document.getElementById('prev');
 const next = document.getElementById('next');
-let year = document.getElementById('year');
-let month = document.getElementById('month');
-let days = document.getElementById('days');
-let month1 = document.getElementById('month1');
-let month2 = document.getElementById('month2');
-let month3 = document.getElementById('month3');
-let month4 = document.getElementById('month4');
-let month5 = document.getElementById('month5');
-let month6 = document.getElementById('month6');
-let month7 = document.getElementById('month7');
-let month8 = document.getElementById('month8');
-let month9 = document.getElementById('month9');
-let month10 = document.getElementById('month10');
-let month11 = document.getElementById('month11');
-let month12 = document.getElementById('month12');
-thisYear.addEventListener('click', function () {
-    displayCalendar(displayYear = now.getFullYear());
-});
-thisMonth.addEventListener('click', function () { });
-prev.addEventListener('click', function () {
-    displayCalendar(displayYear -= 1);
-});
-next.addEventListener('click', function () {
-    displayCalendar(displayYear += 1);
-});
+const year = document.getElementById('year');
+const month = document.getElementById('month');
+const days = document.getElementById('days');
+const oneYear = document.getElementById('one-year');
+const oneMonth = document.getElementById('one-month');
+const months = document.getElementById('one-year').querySelectorAll('div');
 const now = new Date();
-let displayYear = now.getFullYear();
-displayCalendar(displayYear);
+let displayDate = new Date();
+let displayMode = 'year';
+
+thisYear.addEventListener('click', function() {
+    oneMonth.classList.remove('active');
+    oneYear.classList.add('active');
+    displayMode = 'year';
+    displayDate = new Date();
+    displayCalendar(displayDate.getFullYear());
+});
+thisMonth.addEventListener('click', function() {
+    oneYear.classList.remove('active');
+    oneMonth.classList.add('active');
+    displayMode = 'month';
+    displayDate = new Date();
+    displayDate.setDate(1);
+    year.innerHTML = displayDate.getFullYear() + '年';
+    oneMonth.innerHTML = displayMonth(displayDate);
+});
+prev.addEventListener('click', function() {
+    if (displayMode === 'year') {
+        displayDate.setFullYear(displayDate.getFullYear() - 1);
+        displayCalendar(displayDate.getFullYear());
+    } else if (displayMode === 'month') {
+        displayDate.setMonth(displayDate.getMonth() - 2);
+        year.innerHTML = displayDate.getFullYear() + '年';
+        oneMonth.innerHTML = displayMonth(displayDate);
+    }
+});
+next.addEventListener('click', function() {
+    if (displayMode === 'year') {
+        displayDate.setFullYear(displayDate.getFullYear() + 1);
+        displayCalendar(displayDate.getFullYear());
+    } else if (displayMode === 'month') {
+        year.innerHTML = displayDate.getFullYear() + '年';
+        oneMonth.innerHTML = displayMonth(displayDate);
+    }
+});
+
+months.forEach(function(el, i) {
+    el.addEventListener('click', function() {
+        if (displayMode === 'year') {
+            oneYear.classList.remove('active');
+            oneMonth.classList.add('active');
+            displayMode = 'month';
+            displayDate.setMonth(i);
+            displayDate.setDate(1);
+            oneMonth.innerHTML = displayMonth(displayDate);
+        }
+    });
+});
+
+displayCalendar(displayDate.getFullYear()); //最初にカレンダーを表示する
 
 function displayCalendar(setCalendarYear) {
-    let calendar = [];
+    const calendar = [];
     for (let i = 0; i < 12; i++) {
-        // month1.style.visibility = 'hidden';
-        // month2.style.visibility = 'hidden';
-        // month3.style.visibility = 'hidden';
-        // month4.style.visibility = 'hidden';
-        // month5.style.visibility = 'hidden';
-        // month6.style.visibility = 'hidden';
-        // month7.style.visibility = 'hidden';
-        // month8.style.visibility = 'hidden';
-        // month9.style.visibility = 'hidden';
-        // month10.style.visibility = 'hidden';
-        // month11.style.visibility = 'hidden';
-        // month12.style.visibility = 'hidden';
         calendar[i] = new Date(setCalendarYear, 0);
         year.innerHTML = calendar[i].getFullYear() + '年';
         calendar[i].setDate(1);
         calendar[i].setMonth(i);
-        switch (i) {
-            case 0:
-                month1.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 1:
-                month2.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 2:
-                month3.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 3:
-                month4.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 4:
-                month5.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 5:
-                month6.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 6:
-                month7.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 7:
-                month8.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 8:
-                month9.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 9:
-                month10.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 10:
-                month11.innerHTML = displayMonth(calendar[i]);
-                break;
-            case 11:
-                month12.innerHTML = displayMonth(calendar[i]);
-                break;
-        }
+        months[i].innerHTML = displayMonth(calendar[i]);
     }
+}
 
-    function displayMonth(calendar) {
-        let calendarTable = '';
-        let calendarMonth = calendar.getMonth();
-        let calendarDay = calendar.getDay();
-        calendarTable += '<table><caption>' + (calendar.getMonth() + 1) + '月</caption>';
-        calendarTable += '<colgroup id="sun" style="background-color:orange;"></colgroup><colgroup id="mon"></colgroup><colgroup id="tus"></colgroup><colgroup id="wen"></colgroup><colgroup id="thu"></colgroup><colgroup id="fri"></colgroup><colgroup id="sat" style="background-color:aqua;"></colgroup>'
-        calendarTable += '<!--<thead><tr><th style="color:red;">日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th style="color:blue;">土</th></tr></thead>--><tbody>';
-        for (let i = 0; i < 42; i++) {
-            if (i % 7 === 0) {
-                calendarTable += '<tr>';
-            }
-            if (i >= calendarDay && calendarMonth === calendar.getMonth()) {
-                if (now.getFullYear() === calendar.getFullYear() && now.getMonth() === calendar.getMonth() && now.getDate() === calendar.getDate()) {
-                    // 今日を表示
-                    calendarTable += '<td style="font-weight:bold;color:red;background-color:yellow;">' + calendar.getDate() + '</td>';
-                } else {
-                    calendarTable += '<td>' + calendar.getDate() + '</td>';
-                }
-                calendar.setTime(calendar.getTime() + 86400000);
-            } else {
-                calendarTable += '<td>　</td>';
-            }
-            if (i % 7 === 6) {
-                calendarTable += '</tr>';
-            }
-        }
-        calendarTable += '</tbody></table>';
-        console.log(calendarTable);
-        return calendarTable;
+function displayMonth(calendar) {
+    let calendarTable = '';
+    const calendarMonth = calendar.getMonth();
+    const calendarDay = calendar.getDay();
+    calendarTable += '<table><caption>' + (calendar.getMonth() + 1) + '月</caption>';
+    calendarTable += '<colgroup id="sun" style="background-color:orange;"></colgroup><colgroup id="mon"></colgroup><colgroup id="tus"></colgroup><colgroup id="wen"></colgroup><colgroup id="thu"></colgroup><colgroup id="fri"></colgroup><colgroup id="sat" style="background-color:aqua;"></colgroup>'
+    if (displayMode === 'month') {
+        calendarTable += '<thead><tr><th style="color:red;">日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th style="color:blue;">土</th></tr></thead>';
     }
-    // setTimeout(function(){
-    //     month1.style.visibility = 'visible';
-    // }, 20);
-    // setTimeout(function(){
-    //     month2.style.visibility = 'visible';
-    // }, 40);
-    // setTimeout(function(){
-    //     month3.style.visibility = 'visible';
-    // }, 60);
-    // setTimeout(function(){
-    //     month4.style.visibility = 'visible';
-    // }, 80);
-    // setTimeout(function(){
-    //     month5.style.visibility = 'visible';
-    // }, 100);
-    // setTimeout(function(){
-    //     month6.style.visibility = 'visible';
-    // }, 120);
-    // setTimeout(function(){
-    //     month7.style.visibility = 'visible';
-    // }, 140);
-    // setTimeout(function(){
-    //     month8.style.visibility = 'visible';
-    // }, 160);
-    // setTimeout(function(){
-    //     month9.style.visibility = 'visible';
-    // }, 180);
-    // setTimeout(function(){
-    //     month10.style.visibility = 'visible';
-    // }, 200);
-    // setTimeout(function(){
-    //     month11.style.visibility = 'visible';
-    // }, 220);
-    // setTimeout(function(){
-    //     month12.style.visibility = 'visible';
-    // }, 240);
+    calendarTable += '<tbody>';
+    for (let i = 0; i < 42; i++) {
+        if (i % 7 === 0) {
+            calendarTable += '<tr>';
+        }
+        if (i >= calendarDay && calendarMonth === calendar.getMonth()) {
+            if (now.getFullYear() === calendar.getFullYear() && now.getMonth() === calendar.getMonth() && now.getDate() === calendar.getDate()) {
+                // 今日を表示
+                calendarTable += '<td style="font-weight:bold;color:red;background-color:yellow;">' + calendar.getDate() + '</td>';
+            } else {
+                calendarTable += '<td>' + calendar.getDate() + '</td>';
+            }
+            calendar.setTime(calendar.getTime() + 86400000);
+        } else {
+            calendarTable += '<td>　</td>';
+        }
+        if (i % 7 === 6) {
+            calendarTable += '</tr>';
+        }
+    }
+    calendarTable += '</tbody></table>';
+    console.log(calendarTable);
+    return calendarTable;
 }
