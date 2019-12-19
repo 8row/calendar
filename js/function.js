660de4ec49db7b9517aa0af0d600f7d8f4102a40
masuda8row <masuda8row@gmail.com>
2019-12-19 21:02:51 +0900

    Update function.js

--- js/function.js
+++ js/function.js
@@ -15,44 +15,43 @@ const now = new Date();
 let displayDate = new Date();
 let displayMode = 'year';
 
-thisYear.addEventListener('click', function() {
+thisYear.addEventListener('click', function () {
     oneMonth.classList.remove('active');
     oneYear.classList.add('active');
     displayMode = 'year';
     displayDate = new Date();
     displayCalendar(displayDate.getFullYear());
 });
-thisMonth.addEventListener('click', function() {
+thisMonth.addEventListener('click', function () {
     oneYear.classList.remove('active');
     oneMonth.classList.add('active');
     displayMode = 'month';
     displayDate = new Date();
     displayDate.setDate(1);
-    year.innerHTML = displayDate.getFullYear() + '年';
+    year.innerHTML = `${displayDate.getFullYear()}年`;
     oneMonth.innerHTML = displayMonth(displayDate);
 });
-prev.addEventListener('click', function() {
+prev.addEventListener('click', function () {
     if (displayMode === 'year') {
         displayDate.setFullYear(displayDate.getFullYear() - 1);
         displayCalendar(displayDate.getFullYear());
     } else if (displayMode === 'month') {
         displayDate.setMonth(displayDate.getMonth() - 2);
-        year.innerHTML = displayDate.getFullYear() + '年';
+        year.innerHTML = `${displayDate.getFullYear()}年`;
         oneMonth.innerHTML = displayMonth(displayDate);
     }
 });
-next.addEventListener('click', function() {
+next.addEventListener('click', function () {
     if (displayMode === 'year') {
         displayDate.setFullYear(displayDate.getFullYear() + 1);
         displayCalendar(displayDate.getFullYear());
     } else if (displayMode === 'month') {
-        year.innerHTML = displayDate.getFullYear() + '年';
+        year.innerHTML = `${displayDate.getFullYear()}年`;
         oneMonth.innerHTML = displayMonth(displayDate);
     }
 });
-
-months.forEach(function(el, i) {
-    el.addEventListener('click', function() {
+months.forEach(function (el, i) {
+    el.addEventListener('click', function () {
         if (displayMode === 'year') {
             oneYear.classList.remove('active');
             oneMonth.classList.add('active');
@@ -63,14 +62,13 @@ months.forEach(function(el, i) {
         }
     });
 });
-
 displayCalendar(displayDate.getFullYear()); //最初にカレンダーを表示する
 
 function displayCalendar(setCalendarYear) {
     const calendar = [];
     for (let i = 0; i < 12; i++) {
         calendar[i] = new Date(setCalendarYear, 0);
-        year.innerHTML = calendar[i].getFullYear() + '年';
+        year.innerHTML = `${calendar[i].getFullYear()}年`;
         calendar[i].setDate(1);
         calendar[i].setMonth(i);
         months[i].innerHTML = displayMonth(calendar[i]);
@@ -81,32 +79,32 @@ function displayMonth(calendar) {
     let calendarTable = '';
     const calendarMonth = calendar.getMonth();
     const calendarDay = calendar.getDay();
-    calendarTable += '<table><caption>' + (calendar.getMonth() + 1) + '月</caption>';
-    calendarTable += '<colgroup id="sun" style="background-color:orange;"></colgroup><colgroup id="mon"></colgroup><colgroup id="tus"></colgroup><colgroup id="wen"></colgroup><colgroup id="thu"></colgroup><colgroup id="fri"></colgroup><colgroup id="sat" style="background-color:aqua;"></colgroup>'
+    calendarTable += `<table><caption>${(calendar.getMonth() + 1)}月</caption>`;
+    calendarTable += `<colgroup id="sun" style="background-color:orange;"></colgroup><colgroup id="mon"></colgroup><colgroup id="tus"></colgroup><colgroup id="wen"></colgroup><colgroup id="thu"></colgroup><colgroup id="fri"></colgroup><colgroup id="sat" style="background-color:aqua;"></colgroup>`;
     if (displayMode === 'month') {
-        calendarTable += '<thead><tr><th style="color:red;">日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th style="color:blue;">土</th></tr></thead>';
+        calendarTable += `<thead><tr><th style="color:red;">日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th style="color:blue;">土</th></tr></thead>`;
     }
-    calendarTable += '<tbody>';
+    calendarTable += `<tbody>`;
     for (let i = 0; i < 42; i++) {
         if (i % 7 === 0) {
-            calendarTable += '<tr>';
+            calendarTable += `<tr>`;
         }
         if (i >= calendarDay && calendarMonth === calendar.getMonth()) {
             if (now.getFullYear() === calendar.getFullYear() && now.getMonth() === calendar.getMonth() && now.getDate() === calendar.getDate()) {
                 // 今日を表示
-                calendarTable += '<td style="font-weight:bold;color:red;background-color:yellow;">' + calendar.getDate() + '</td>';
+                calendarTable += `<td style="font-weight:bold;color:red;background-color:yellow;">${calendar.getDate()}</td>`;
             } else {
-                calendarTable += '<td>' + calendar.getDate() + '</td>';
+                calendarTable += `<td>${calendar.getDate()}</td>`;
             }
             calendar.setTime(calendar.getTime() + 86400000);
         } else {
-            calendarTable += '<td>　</td>';
+            calendarTable += `<td>　</td>`;
         }
         if (i % 7 === 6) {
-            calendarTable += '</tr>';
+            calendarTable += `</tr>`;
         }
     }
-    calendarTable += '</tbody></table>';
+    calendarTable += `</tbody></table>`;
     console.log(calendarTable);
     return calendarTable;
 }
